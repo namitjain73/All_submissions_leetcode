d34e865b0276a04f9cc80bf6b659e5041d4ad927
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:50:51 +0530

    Create 257-BinaryTreePaths

---
+++ 257-BinaryTreePaths/257-BinaryTreePaths.java
@@ -0,0 +1,40 @@
+// Last updated: 8/28/2025, 2:50:44 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void paths(TreeNode root,List<String> ans, List<String> path){
+        if(root == null) return;
+        path.add(Integer.toString(root.val));
+        if(root.left == null && root.right == null){
+            ans.add(String.join("->" , path));
+        
+        }
+        else{
+            paths(root.left,ans,path);
+            paths(root.right,ans,path);
+        }
+        path.remove(path.size()-1);
+
+        
+    }
+    public List<String> binaryTreePaths(TreeNode root) {
+        
+        List<String> ans = new ArrayList<>();
+        List<String> path = new ArrayList<>();
+        paths(root,ans,path);
+        return ans;
+    }
+}
