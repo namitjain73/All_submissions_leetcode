d6fa2a6c551d7d461ccb0ce8aeff0952f74a36e7
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:47:23 +0530

    Create 2204-FindSubsequenceOfLengthKWithTheLargestSum

---
+++ 2204-FindSubsequenceOfLengthKWithTheLargestSum/2204-FindSubsequenceOfLengthKWithTheLargestSum.java
@@ -0,0 +1,23 @@
+// Last updated: 8/28/2025, 2:47:16 AM
+class Solution {
+    public int[] maxSubsequence(int[] nums, int k) {
+        int[] arr = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(arr);
+        List<Integer> list = new ArrayList<>();
+        for(int i = arr.length-1 ; i >= nums.length - k  ; i--){
+            list.add(arr[i]);
+        }
+
+        int[] arr1 = new int[k];
+        int j = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(list.contains(nums[i])){
+                arr1[j] = nums[i];
+                list.remove((Integer)nums[i]);
+                j++;
+            }
+        }
+        System.out.print(list);
+        return arr1;
+    }
+}
