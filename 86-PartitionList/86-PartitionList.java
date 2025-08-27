732d70a89784aaf160015af69c437da6eb7c9105
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:52:13 +0530

    Create 86-PartitionList

---
+++ 86-PartitionList/86-PartitionList.java
@@ -0,0 +1,36 @@
+// Last updated: 8/28/2025, 2:52:06 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode left = new ListNode();
+        ListNode right = new ListNode();
+        ListNode lefttemp = left;
+        ListNode righttemp = right;
+        while(head != null){
+            if(head.val < x){
+                left.next = head;
+                left = left.next;
+            }
+            else{
+                right.next = head;
+                right = right.next;
+            }
+            head  = head.next;
+        }
+        
+            right.next = null;
+        left.next = righttemp.next;
+        return lefttemp.next;
+
+
+    }
+}
