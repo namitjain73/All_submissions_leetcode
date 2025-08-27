5161d70c6db90d68e134509d3c309c97a8417dcb
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:48:47 +0530

    Create 952-WordSubsets

---
+++ 952-WordSubsets/952-WordSubsets.java
@@ -0,0 +1,39 @@
+// Last updated: 8/28/2025, 2:48:40 AM
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int[] frearr = new int[26];
+        int[] maxarr = new int[26];
+
+        for(String word : words2){
+            Arrays.fill(frearr,0);
+            for(char ch : word.toCharArray()){
+                frearr[ch - 'a']++;
+            }
+            for(int i = 0 ; i < 26 ; i++){
+                maxarr[i] = Math.max(maxarr[i] , frearr[i]);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        for(String word : words1){
+            Arrays.fill(frearr,0);
+            for(char ch : word.toCharArray()){
+                frearr[ch - 'a']++;
+            }
+
+            boolean flag = true;
+            for(int i = 0 ;i < 26 ; i++){
+                if(frearr[i] < maxarr[i]){
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag){
+                list.add(word);
+            }
+        }
+        return list;
+        
+    }
+}
