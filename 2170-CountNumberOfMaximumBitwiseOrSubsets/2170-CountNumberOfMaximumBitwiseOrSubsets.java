9b1287bf1751b1daa33b9c81456ce1edbebc9321
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:47:25 +0530

    Create 2170-CountNumberOfMaximumBitwiseOrSubsets

---
+++ 2170-CountNumberOfMaximumBitwiseOrSubsets/2170-CountNumberOfMaximumBitwiseOrSubsets.java
@@ -0,0 +1,20 @@
+// Last updated: 8/28/2025, 2:47:17 AM
+class Solution {
+    public int countMaxOrSubsets(int[] nums) {
+        int max = 0;
+        for(int i : nums) max |= i;
+        int c = solve(nums,max,0,0,0);
+        return c;
+    }
+    public int solve(int[] nums , int max , int curr , int i,int c){
+        if(i >= nums.length){
+            if(curr == max) return 1;
+            return 0;
+        }
+
+        int n = nums[i];
+        int exclude = solve(nums,max,curr,i+1,c);
+        int include= solve(nums ,  max , curr | n , i+1 , c);
+        return include + exclude;
+    }
+}
