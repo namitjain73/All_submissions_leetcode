98ebb7874bd3097c71350743f6b2b39a1f6eb3a0
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:48:07 +0530

    Create 1510-FindLuckyIntegerInAnArray

---
+++ 1510-FindLuckyIntegerInAnArray/1510-FindLuckyIntegerInAnArray.java
@@ -0,0 +1,20 @@
+// Last updated: 8/28/2025, 2:47:59 AM
+class Solution {
+    public int findLucky(int[] arr) {
+        Map<Integer , Integer> map = new HashMap<>();
+
+        for(int i = 0 ; i < arr.length ; i++){
+            map.put(arr[i], map.getOrDefault(arr[i] , 0) + 1);
+        }
+
+        int res = -1;
+        for (int key : map.keySet()) {
+        if (key == map.get(key)) {
+            res = Math.max(res, key);
+        }
+    }
+
+    return res;
+        
+    }
+}
