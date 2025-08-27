ec4835097b134dec5c5a1f768894918435f2ecbd
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:52:28 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,17 @@
+// Last updated: 8/28/2025, 2:52:21 AM
+class Solution {
+    public String addBinary(String a, String b) {
+        int n = a.length()-1;
+        int m = b.length()-1;
+        int c = 0;
+        StringBuilder sb = new StringBuilder();
+        while(n >= 0 || m >= 0 || c != 0){
+            int bita = (n >= 0) ? a.charAt(n--) - '0' : 0;
+            int bitb = (m >= 0) ? b.charAt(m--) - '0' : 0;
+            int sum = bita + bitb + c;
+            c = sum / 2;
+            sb.append(sum % 2);
+        }
+        return sb.reverse().toString();
+    }
+}
