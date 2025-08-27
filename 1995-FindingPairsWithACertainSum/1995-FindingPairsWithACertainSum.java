57fb707335fc8cd2dd8b9abe23ea101f0941261c
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:47:35 +0530

    Create 1995-FindingPairsWithACertainSum

---
+++ 1995-FindingPairsWithACertainSum/1995-FindingPairsWithACertainSum.java
@@ -0,0 +1,28 @@
+// Last updated: 8/28/2025, 2:47:28 AM
+class FindSumPairs {
+    Map<Integer,Integer> map = new HashMap<>();
+    int[] arr1;
+    int[] arr2;
+    public FindSumPairs(int[] nums1, int[] nums2) {
+        arr1 = nums1;
+        arr2 = nums2;
+        for(int x : nums2){
+            map.put(x,map.getOrDefault(x,0)+1);
+        }
+    }
+    
+    public void add(int index, int val) {
+        map.put(arr2[index],map.get(arr2[index])-1);
+        arr2[index] += val;
+        map.put(arr2[index],map.getOrDefault(arr2[index],0)+1);
+    }
+    
+    public int count(int tot) {
+        int res= 0;
+        for(int i : arr1){
+            res += map.getOrDefault(tot - i,0);
+        }
+
+        return res;
+    }
+}
