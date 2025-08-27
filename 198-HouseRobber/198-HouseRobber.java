dee49c1dc15dcece1dfab9c1533ec8974a1c049d
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:51:21 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.java
@@ -0,0 +1,25 @@
+// Last updated: 8/28/2025, 2:51:14 AM
+class Solution {
+    public int rob(int[] nums) {
+        return solve(nums);
+    }
+
+    public int solve(int [] nums){
+        int n = nums.length;
+
+        if(n == 0) return 0;
+        if(n == 1) return nums[0];
+        
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0],nums[1]);
+
+        for(int i = 2 ; i < n ; i++){
+            int incl = dp[i - 2] + nums[i];
+            int exc = dp[i-1];
+            dp[i] = Math.max(incl,exc);
+        }
+
+        return dp[n-1];
+    }
+}
