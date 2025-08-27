eb579dcb6cf64ff7481401e7b8e5f7a81929cb47
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:48:28 +0530

    Create 1072-NextGreaterNodeInLinkedList

---
+++ 1072-NextGreaterNodeInLinkedList/1072-NextGreaterNodeInLinkedList.java
@@ -0,0 +1,49 @@
+// Last updated: 8/28/2025, 2:48:21 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        int size = size(head);
+        int[] arr = new int[size];
+        Stack<Integer> st  = new Stack<>();
+        for(int i = 0 ; i < size ; i++){
+            arr[i] = head.val;
+            head = head.next;
+        }
+        if(size == 0){
+            return arr;
+        }
+        for(int i = 0 ; i < size ; i++){
+            while(!st.empty() && arr[i] > arr[st.peek()]){
+                arr[st.pop()] = arr[i];
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()){
+            arr[st.pop()] = 0;
+        }
+
+        return arr;
+        
+
+
+    }
+
+    public int size(ListNode head){
+        int n = 0;
+        while(head !=null){
+            n++;
+            head = head.next;
+        }
+        return n;
+    }
+}
