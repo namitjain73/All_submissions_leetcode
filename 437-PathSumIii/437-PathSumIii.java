4472c4808b34e60de0832d064c1c345e349b5b08
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:50:10 +0530

    Create 437-PathSumIii

---
+++ 437-PathSumIii/437-PathSumIii.java
@@ -0,0 +1,34 @@
+// Last updated: 8/28/2025, 2:50:03 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int allpaths(TreeNode root , long t){
+        if(root == null) return 0;
+        int count = 0;
+        if(t == (int)(root.val)){
+            count++;
+        }
+
+        count += allpaths(root.left,t-(int)(root.val)) + allpaths(root.right,t-(int)(root.val));
+        return count;
+    }
+    public int pathSum(TreeNode root, int t) {
+        if(root == null) return 0;
+        int count = allpaths(root,(long)t);
+        count += pathSum(root.left,t) + pathSum(root.right,t);
+        return count;
+    }
+}
