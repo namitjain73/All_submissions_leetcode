2d725722d1001e8bb5487294844a46bf4d1f44b6
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:49:28 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,23 @@
+// Last updated: 8/28/2025, 2:49:20 AM
+class Solution {
+    public int calPoints(String[] s) {
+        int n = 0;
+        List<Integer> ll = new ArrayList<>();
+        for (int i = 0; i < s.length; i++) {
+    if (s[i].equals("C")) {
+        ll.remove(ll.size() - 1);
+    } else if (s[i].equals("D")) {
+        ll.add(ll.get(ll.size() - 1) * 2);
+    } else if (s[i].equals("+")) {
+        ll.add(ll.get(ll.size() - 1) + ll.get(ll.size() - 2));
+    } else {
+        ll.add(Integer.parseInt(s[i]));
+    }
+}
+        for(int i = 0 ;  i < ll.size() ; i++){
+            n += ll.get(i);
+        }
+        return n;
+        
+    }
+}
