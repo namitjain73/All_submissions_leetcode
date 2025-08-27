50acf895b144b493b6e16d7f4cea8e4030177dfa
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:52:39 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,8 @@
+// Last updated: 8/28/2025, 2:52:32 AM
+import static java.lang.Math.*;
+class Solution {
+    public double myPow(double x, int n) {
+        return (double)Math.pow(x,n);
+        
+    }
+}
