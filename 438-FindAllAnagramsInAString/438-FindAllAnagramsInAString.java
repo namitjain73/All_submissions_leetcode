1b65532b5307531b7903824fb4b74a08cf53d9eb
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:50:09 +0530

    Create 438-FindAllAnagramsInAString

---
+++ 438-FindAllAnagramsInAString/438-FindAllAnagramsInAString.java
@@ -0,0 +1,35 @@
+// Last updated: 8/28/2025, 2:50:02 AM
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        int[] par = new int[26];
+        for(int i = 0 ; i < p.length() ; i++){
+            par[p.charAt(i) - 'a']++;
+        }
+        if(p.length() > s.length()){
+            return list;
+        }
+
+        for(int i = 0 ; i < s.length() - p.length() + 1 ; i++){
+            String as = s.substring(i , i+p.length());
+            if(cheak(as , par)){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public boolean cheak(String s , int[] p){
+        int[] str = new int[26];
+        for(int i = 0 ; i < s.length() ; i++){
+            str[s.charAt(i) - 'a']++;
+        }
+
+        for(int i = 0 ; i < 26 ; i++){
+            if(str[i] != p[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
