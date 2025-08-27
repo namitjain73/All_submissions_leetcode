3caa452bda59c49520b5b3e002f46080e4528413
Namit jain <namitjain9838@gmail.com>
2025-08-28 02:48:48 +0530

    Create 948-SortAnArray

---
+++ 948-SortAnArray/948-SortAnArray.java
@@ -0,0 +1,8 @@
+// Last updated: 8/28/2025, 2:48:41 AM
+import java.util.Arrays;
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
